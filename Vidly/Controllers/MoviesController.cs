b6e965ff80e6191275e98e7600a2fe3346f39196
Abdullah Ejaz <aejaz3@dxc.com>
2020-05-14 16:02:19 -0500

    Add customer table functionality

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -12,11 +14,41 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie()
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
             {
-                Name = "Shrek!"
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
             };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index()
+        {
+            var movie = GetMovies();
             return View(movie);
         }
+        private IEnumerable<Movie> GetMovies()
+        {      
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Star Trek"},
+                new Movie {Id = 2, Name = "Sholay"}
+            };
+        }
+        
     }
 }
